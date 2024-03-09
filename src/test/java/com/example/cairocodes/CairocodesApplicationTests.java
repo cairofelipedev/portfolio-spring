f61445514d15fc005036ee20ca25812ff9a76570
Cairo <cairofelipedev@gmail.com>
2024-03-09 17:28:19 -0300

    first commit

---
+++ src/test/java/com/example/cairocodes/CairocodesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cairocodes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CairocodesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
